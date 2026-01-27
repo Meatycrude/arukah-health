bd188bcdc6028f96e6a88ba6fd01e38867ec442a
harrison opondo otieno <159556298+Meatycrude@users.noreply.github.com>
2026-01-27 14:25:32 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
-import About from "@/components/AboutUs";
+
 export default function HomePage() {
   return (
     <main className="relative mx-auto min-h-screen ">
       <Navbar />
       <Hero />
-      <About />
     </main>
   );
 }
